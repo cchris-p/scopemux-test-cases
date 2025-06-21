c2156ca3d339dc7ab6735301bcbbf731c6fe7812
cchrisp <rarityrings333@gmail.com>
2025-06-21 21:12:08 +0000

    feat: implement config-driven node type mapping for Tree-sitter queries

---
+++ python/basic_syntax/classes.py
@@ -0,0 +1,8 @@
+class SimpleClass:
+    pass
+
+class ClassWithMethods:
+    def __init__(self):
+        pass
+    def method_one(self):
+        return 1
---
+++ python/basic_syntax/functions.py
@@ -0,0 +1,9 @@
+def simple_function():
+    pass
+
+def function_with_parameters(a, b):
+    return a + b
+
+def function_with_docstring():
+    """This is a docstring."""
+    return 42
